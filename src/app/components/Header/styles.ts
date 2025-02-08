b29b87efd3cac78307eb40b8e78c948bfb9d21c9
Elisson Victor <elissonvictor.01@hotmail.com>
2025-02-08 19:27:45 -0300

    upd

--- src/app/components/Header/styles.ts
+++ src/app/components/Header/styles.ts
@@ -1,26 +1,20 @@
 import { styled } from 'styled-components'
 
 export const Head = styled.header`
-  height: 6rem;
   display: flex;
   align-items: center;
-
   padding: 1rem 2rem;
   justify-content: space-between;
-
+  user-select: none;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
   & .logo {
     display: flex;
     align-items: center;
-    gap: 12px;
+    gap: 0.75rem;
   }
 
   & .logoInfo {
-    display:flex;
-    flex-flow: column;
-    height: min-content;
-
     #titulo {
       font-size: 2.25rem;
     }
@@ -38,23 +32,29 @@ export const Head = styled.header`
     
     & a {
       font-size: 1.25rem;
+      transition: 0.3s ease-in-out;
+
+    &:hover {
+      color: #154277;
     }
+  }
+
   }
 
   & button {
     background-color: #4a90e2;
     color: white;
     height: min-content;
-    padding: 0.8rem 1.125rem;
+    padding: 0.75rem 1rem;
     border: 0;
     border-radius: 8px;
     font-size: 1.25rem;
     text-transform: uppercase;
     cursor: pointer;
+    transition: 0.3s ease-in-out; // Coloca a transition no próprio botão, a animação fica mais clean!
 
     &:hover {
       background-color: #154277;
-      transition: 0.2s;
     }
   }
 `;
